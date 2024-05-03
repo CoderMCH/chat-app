91625e5b547fb5726ea0d8b736aff70860655ce8
WIN-20230818KUB\OWNER <manchinghin@gmail.com>
2024-05-03 08:09:49 -0400

    1. expo template 2. Start page 3. Chat page

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Chat } from './components/Chat';
+import { Start } from './components/Start';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    return (
+        <NavigationContainer style={styles.container}>
+            <Stack.Navigator initialRouteName="Start">
+                <Stack.Screen name="Start" component={Start} />
+                <Stack.Screen name="Chat" component={Chat} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+    }
+
+    const styles = StyleSheet.create({
+        container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
